c9f46df4163b762481a8699fd2e6129a0d1c78df
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2023-01-17 14:57:41 +0300

    Update AddTransaction.aspx.cs

--- AddTransaction.aspx.cs
+++ AddTransaction.aspx.cs
@@ -38,92 +38,6 @@ namespace AIU_ATM
             }
         }
 
-        protected bool notEmpty()
-        {
-            bool res = true;
-            if (TextBoxAccountNo.Text != "")
-            {
-                bool r = true;
-                TextBoxAccountNo.Text = TextBoxAccountNo.Text.ToString().Trim();
-                try
-                {
-                    float number = float.Parse(TextBoxAccountNo.Text);
-                }
-                catch (Exception ex)
-                {
-                    r = false;
-                }
-                TextBoxAccountNo.CssClass = "form-control";
-                if (!r)
-                {
-                    LabelAccountNoFeedback.Text = "Account number can contain numbers only";
-                    TextBoxAccountNo.CssClass = "form-control is-invalid";
-                }
-                res = res && r;
-            }
-            else
-            {
-                res = false;
-                LabelAccountNoFeedback.Text = "Required";
-                TextBoxAccountNo.CssClass = "form-control is-invalid";
-            }
-
-            if (TextBoxAmount.Text != "")
-            {
-                bool r = true;
-                TextBoxAmount.Text = TextBoxAmount.Text.ToString().Trim();
-                try
-                {
-                    float number = float.Parse(TextBoxAmount.Text);
-                }
-                catch (Exception ex)
-                {
-                    r = false;
-                }
-                TextBoxAmount.CssClass = "form-control";
-                if (!r)
-                {
-                    LabelAmountFeedback.Text = "Amount can contain numbers only";
-                    TextBoxAmount.CssClass = "form-control is-invalid";
-                }
-                res = res && r;
-            }
-            else
-            {
-                res = false;
-                LabelAmountFeedback.Text = "Required";
-                TextBoxAmount.CssClass = "form-control is-invalid";
-            }
-
-            if (TextBoxRecipientNo.Text != "")
-            {
-                bool r = true;
-                TextBoxRecipientNo.Text = TextBoxRecipientNo.Text.ToString().Trim();
-                try
-                {
-                    float number = float.Parse(TextBoxRecipientNo.Text);
-                }
-                catch (Exception ex)
-                {
-                    r = false;
-                }
-                TextBoxRecipientNo.CssClass = "form-control";
-                if (!r)
-                {
-                    LabelRecipientNoFeedback.Text = "PIN code only contain numbers";
-                    TextBoxRecipientNo.CssClass = "form-control is-invalid";
-                }
-                res = res && r;
-            }
-            else
-            {
-                res = false;
-                LabelRecipientNoFeedback.Text = "Required";
-                TextBoxRecipientNo.CssClass = "form-control is-invalid";
-            }
-            return res;
-        }
-
         protected void LinkButtonBack_Click(object sender, EventArgs e)
         {
 
@@ -154,7 +68,85 @@ namespace AIU_ATM
 
                 if (selTT == 2)
                 {
-                    if (TextBoxSenderNo.Text != "" && TextBoxRecipientNo.Text != "")
+                    bool res = true;
+                    if (TextBoxSenderNo.Text != "")
+                    {
+                        bool r = true;
+                        TextBoxSenderNo.Text = TextBoxSenderNo.Text.ToString().Trim();
+                        try
+                        {
+                            float number = float.Parse(TextBoxSenderNo.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            r = false;
+                        }
+                        TextBoxSenderNo.CssClass = "form-control";
+                        if (!r)
+                        {
+                            LabelSenderNoFeedback.Text = "Accounts is only numbers";
+                            TextBoxSenderNo.CssClass = "form-control is-invalid";
+                        }
+                        res = res && r;
+                    }
+                    else{
+                        res = false;
+                        LabelSenderNoFeedback.Text = "Required";
+                        TextBoxSenderNo.CssClass = "form-control is-invalid";
+                    }
+                    if (TextBoxRecipientNo.Text != "")
+                    {
+                        bool r = true;
+                        TextBoxRecipientNo.Text = TextBoxRecipientNo.Text.ToString().Trim();
+                        try
+                        {
+                            float number = float.Parse(TextBoxRecipientNo.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            r = false;
+                        }
+                        TextBoxRecipientNo.CssClass = "form-control";
+                        if (!r)
+                        {
+                            LabelRecipientNoFeedback.Text = "Accounts is only numbers";
+                            TextBoxRecipientNo.CssClass = "form-control is-invalid";
+                        }
+                        res = res && r;
+                    }
+                    else
+                    {
+                        res = false;
+                        LabelRecipientNoFeedback.Text = "Required";
+                        TextBoxRecipientNo.CssClass = "form-control is-invalid";
+                    }
+                    if (TextBoxAmount.Text != "")
+                    {
+                        bool r = true;
+                        TextBoxAmount.Text = TextBoxAmount.Text.ToString().Trim();
+                        try
+                        {
+                            float number = float.Parse(TextBoxAmount.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            r = false;
+                        }
+                        TextBoxAmount.CssClass = "form-control";
+                        if (!r)
+                        {
+                            LabelAmountFeedback.Text = "Amount can contain numbers only";
+                            TextBoxAmount.CssClass = "form-control is-invalid";
+                        }
+                        res = res && r;
+                    }
+                    else
+                    {
+                        res = false;
+                        LabelRecipientNoFeedback.Text = "Required";
+                        TextBoxRecipientNo.CssClass = "form-control is-invalid";
+                    }
+                    if (res)
                     {
                         cmd.CommandText = "select * from Accounts where AccountNo=@aNo2";
                         cmd.Parameters.AddWithValue("@aNo2", TextBoxRecipientNo.Text);
@@ -168,8 +160,12 @@ namespace AIU_ATM
 
                         da.Fill(dt);
 
-                        if (ex <= 0) { TextBoxRecipientNo.Text = ""; }
-                        else if (dt.Rows.Count > 0 && TextBoxRecipientNo.Text != "")
+                        if (ex <= 0) 
+                        {                            
+                            LabelRecipientNoFeedback.Text = "Recipient AccountNo doesn't exist";
+                            TextBoxRecipientNo.CssClass = "form-control is-invalid";                            
+                        }
+                        else if (dt.Rows.Count > 0)
                         {
                             balance = double.Parse(dt.Rows[0]["Balance"].ToString());
                             if (amount < balance)
@@ -187,17 +183,76 @@ namespace AIU_ATM
                                 TextBoxRecipientNo.Text = "";
                             }
                         }
+                        else
+                        {
+                            LabelSenderNoFeedback.Text = "Recipient AccountNo doesn't exist";
+                            TextBoxSenderNo.CssClass = "form-control is-invalid";
+                        }
                     }
-                    else { TextBoxRecipientNo.Text = TextBoxSenderNo.Text = ""; }
+                    
                 }
                 else if (selTT == 0 || selTT == 1)
                 {
+                    bool res = true;
+                    if (TextBoxAccountNo.Text != "")
+                    {
+                        bool r = true;
+                        TextBoxAccountNo.Text = TextBoxAccountNo.Text.ToString().Trim();
+                        try
+                        {
+                            float number = float.Parse(TextBoxAccountNo.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            r = false;
+                        }
+                        TextBoxAccountNo.CssClass = "form-control";
+                        if (!r)
+                        {
+                            LabelAccountNoFeedback.Text = "Accounts is only numbers";
+                            TextBoxAccountNo.CssClass = "form-control is-invalid";
+                        }
+                        res = res && r;
+                    }
+                    else
+                    {
+                        res = false;
+                        LabelRecipientNoFeedback.Text = "Required";
+                        TextBoxRecipientNo.CssClass = "form-control is-invalid";
+                    }
+                    if (TextBoxAmount.Text != "")
+                    {
+                        bool r = true;
+                        TextBoxAmount.Text = TextBoxAmount.Text.ToString().Trim();
+                        try
+                        {
+                            float number = float.Parse(TextBoxAmount.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            r = false;
+                        }
+                        TextBoxAmount.CssClass = "form-control";
+                        if (!r)
+                        {
+                            LabelAmountFeedback.Text = "Amount can contain numbers only";
+                            TextBoxAmount.CssClass = "form-control is-invalid";
+                        }
+                        res = res && r;
+                    }
+                    else
+                    {
+                        res = false;
+                        LabelAccountNoFeedback.Text = "Required";
+                        TextBoxAmount.CssClass = "form-control is-invalid";
+                    }
+
                     double balance = 0;
                     cmd.CommandText = "select * from Accounts where AccountNo=@AN";
                     cmd.Parameters.AddWithValue("@AN", TextBoxAccountNo.Text);
 
                     da.Fill(dt);
-
+                    if (!res) { return; }
                     if (dt.Rows.Count > 0)
                     {
                         balance = double.Parse(dt.Rows[0]["Balance"].ToString());
@@ -215,6 +270,10 @@ namespace AIU_ATM
                                 Response.Redirect("AddTransaction.aspx");
                                 TextBoxAmount.Text = "";
                             }
+                            else {
+                                LabelAmountFeedback.Text = "Amount is higher than balace";
+                                TextBoxAmount.CssClass = "form-control is-invalid";
+                            }
                         }
                         else if (selTT == 0)
                         {
@@ -230,13 +289,25 @@ namespace AIU_ATM
                                 Response.Redirect("AddTransaction.aspx");
                                 TextBoxAmount.Text = "";
                             }
+                            else
+                            {
+                                LabelAmountFeedback.Text = "Amount is negative";
+                                TextBoxAmount.CssClass = "form-control is-invalid";
+                            }
                         }
                         TextBoxAccountNo.Text = ""; TextBoxAmount.Text = "";
                     }
-                    else { TextBoxAccountNo.Text = ""; }
+                    else {
+                        LabelAccountNoFeedback.Text = "AccountNo doesn't exist";
+                        TextBoxAmount.CssClass = "form-control is-invalid";
+                    }
                 }
             }
-            else { TextBoxAmount.Text = ""; }
+            else
+            {
+                LabelAmountFeedback.Text = "Amount is negative";
+                TextBoxAmount.CssClass = "form-control is-invalid";
+            }
         }
 
         protected void LinkButtonPrint_Click(object sender, EventArgs e)
